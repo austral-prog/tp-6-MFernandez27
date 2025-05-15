d68003d6bf4f45ded2a81dc2fdecf51672291ad2
MFernandez27 <mfernandez27@mail.austral.edu.ar>
2025-05-15 18:17:04 -0300

    Update lists.py

--- lists.py
+++ lists.py
@@ -1,20 +1,32 @@
-# Replace the "ANSWER HERE" with your answer
-
 def remove_elements(list_to_remove_elements):
-    return "ANSWER HERE"  # Remove this line and implement
-
+    del nombres[0]
+    del nombres[3]
+    del nombres[3]
+    return nombres
 
 def add_elements(list_to_add_elements):
-    return "ANSWER HERE"  # Remove this line and implement
+    apellidos.insert(0, 'Perez')
+    apellidos.insert(7, 'Gomez')
+    return apellidos 
 
 
 def is_empty(list_to_check):
-    return "ANSWER HERE"  # Remove this line and implement
-
+    if len(lista) == 0:
+        return lista
 
 def check_lists(list_to_compare1, list_to_compare2):
-    return "ANSWER HERE"  # Remove this line and implement
-
+    if len(apodos)>2 and len(nombresh)>2:
+        return apodos[2] == nombresh[2]
+      
 
 def list_of_lists(list_of_lists_to_modify):
-    return "ANSWER HERE"  # Remove this line and implement
+    if len(familia[0]) > 2 and len(familia[1]) > 4: 
+        return familia[0][:2] , familia[1][1:4] , familia[2][-2:]
+
+nombres = ['Marina', 'Mora', 'Milagros', 'Mia', 'Maggie', 'Manuela']
+apellidos = ['Fernandez', 'Fiore', 'Fiorentino', 'Ferrari']
+lista = ['']
+apodos = ['Rufi', 'Lola', 'Camilos', 'Ferno', 'Mataholic', 'Angel']
+nombresh = ['Marcelo', 'Marcos', 'Camilo', 'Milo', 'Monse']
+familia = [['Milagros','Santiago','Benicio','Dolores','Nahuel'], ['Matias','Sonia','Nina','Annie','Milagros','Santiago'], ['Tigra','Nicky','Tomi','Vainilla','Licha','Leon']]
+
